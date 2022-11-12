b327979801f26115afd53242451a78cc6ac267f4
Owen Rodriguez <2828505+orodri@users.noreply.github.com>
2022-11-12 18:53:27 -0500

    Add empty iOS project

---
+++ frontend/btSchoolSafety/btSchoolSafety/btSchoolSafetyApp.swift
@@ -0,0 +1,17 @@
+//
+//  btSchoolSafetyApp.swift
+//  btSchoolSafety
+//
+//  Created by Owen Rodriguez on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct btSchoolSafetyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
