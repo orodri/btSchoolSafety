399a8e0037fc47a06c41d719361dfcc4671bd481
Owen Rodriguez <2828505+orodri@users.noreply.github.com>
2022-11-13 16:36:45 -0500

    Stub home screen

---
+++ frontend/btSchoolSafety/btSchoolSafety/HomeView.swift
@@ -0,0 +1,36 @@
+//
+//  HomeView.swift
+//  btSchoolSafety
+//
+//  Created by Owen Rodriguez on 11/12/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("Your location is not being shared right now.")
+            Spacer()
+            Text("Your app permissions are set correctly. ✅")
+            Spacer()
+            Button("PANIC") {
+                
+            }
+            .buttonStyle(.bordered)
+            .tint(.red)
+            .controlSize(.large)
+            Spacer()
+            Text("Hold the panic button for 3 seconds to activate.")
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
