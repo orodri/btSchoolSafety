a5c729ec48fd03633b84ac758da20b3b6be4cacb
angelhyc <angelhyc@umich.edu>
2022-11-14 13:12:26 -0500

    holding button + settings button

--- frontend/btSchoolSafety/btSchoolSafety/HomeView.swift
+++ frontend/btSchoolSafety/btSchoolSafety/HomeView.swift
@@ -6,11 +6,13 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 struct HomeView: View {
     
     @ObservedObject private var system = System.shared
     @State private var isPresenting = false
+    private let locationManager = CLLocationManager();
     
     var body: some View {
         if (system.isWaitingForActivation) {
@@ -19,18 +21,36 @@ struct HomeView: View {
             VStack {
                 Spacer()
                 Text("Your location is not being shared right now.")
+                Button(action: {
+                    switch locationManager.authorizationStatus {
+                        case .notDetermined:
+                            locationManager.requestWhenInUseAuthorization()
+                        case .authorizedAlways, .authorizedWhenInUse:
+                            print("start ranging beacons")
+                //                                locationManager.startRangingBeacons(satisfying: matchingUuid)
+                        default:
+                            print("no auth")
+                        }
+                        UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+                }){ Text("Go to settings...").underline() }
+                // TODO: ^^ This button needs to be hidden when locations are set right
                 Spacer()
                 Text("Your app permissions are set correctly. ✅")
                 Spacer()
-                Button("PANIC") {
+                Button(action:{}) {
+                    Text("**Panic Button**")
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 300)
+                    .background(RoundedRectangle(cornerRadius: 300).fill(Color.red).shadow(radius: 3))
+                                        .font(.largeTitle)
+                }.simultaneousGesture(LongPressGesture(minimumDuration: 3.0).onEnded({_ in
                     isPresenting.toggle()
-                }
-                .buttonStyle(.bordered)
-                .tint(.red)
-                .controlSize(.large)
+                }))
+//                .buttonStyle(.bordered)
+//                .tint(.red)
+//                .controlSize(.large)
                 Spacer()
                 Text("Hold the panic button for 3 seconds to activate.")
-                Spacer()
             }
             .padding()
             .fullScreenCover(isPresented: $isPresenting) {
