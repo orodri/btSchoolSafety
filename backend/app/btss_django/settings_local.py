f38ea41c0653f58c20f20f1f8ae9128f04f4abc8
Owen Rodriguez <2828505+orodri@users.noreply.github.com>
2022-12-05 17:24:30 -0500

    Add navigable map

--- backend/app/btss_django/settings_local.py
+++ backend/app/btss_django/settings_local.py
@@ -136,6 +136,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
