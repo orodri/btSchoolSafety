6ba311cd68e4f769126df9a6cefa721c655b39de
Owen Rodriguez <2828505+orodri@users.noreply.github.com>
2022-12-06 18:57:10 -0500

    Add handling of broken WebSocket connection
    Attempt to reconnect.

--- frontend/btSchoolSafety/btSchoolSafety/LocationTrackingService.swift
+++ frontend/btSchoolSafety/btSchoolSafety/LocationTrackingService.swift
@@ -3,6 +3,7 @@
 //  btSchoolSafety
 //
 //  Created by Owen Rodriguez on 12/3/22.
+//  Shout out to: https://stackoverflow.com/a/69784743
 //
 
 import Foundation
@@ -10,7 +11,11 @@ import Combine
 
 class LocationTrackingService: NSObject, ObservableObject, URLSessionDelegate {
     private var webSocketTask: URLSessionWebSocketTask?
-    private var isOpened = false
+    private var isConnected = false
+    private var isAttemptingReconnect = false
+    private var reconnectTimer: Timer?
+    private var untilNextAttempt = TimeInterval(1)
+    private var maxTimeToWaitBeforeNextAttempt = TimeInterval(8)
     private var subscriptions = Set<AnyCancellable>()
     private var previousNearestBeaconMinor: Int?
     
@@ -18,30 +23,29 @@ class LocationTrackingService: NSObject, ObservableObject, URLSessionDelegate {
     
     private override init() {
         super.init()
-        openWebSocket()
+        connect()
     }
     
-    private func openWebSocket() {
+    private func connect() {
         let url = serverWsUrl.appendingPathComponent("/ws/student")
         let request = URLRequest(url: url)
         let session = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
         
+        print("connecting to \(url)")
         webSocketTask = session.webSocketTask(with: request)
         webSocketTask?.resume()
-        isOpened = true
+        isConnected = true
+        isAttemptingReconnect = false
         
-        self.receiveMessage()
+        self.registerMessageReceiver()
     }
     
-    private func receiveMessage() {
-        if !isOpened {
-            openWebSocket()
-        }
-        
+    private func registerMessageReceiver() {
         webSocketTask?.receive(completionHandler: { result in
             switch result {
-            case .failure(let error):
-                print(error.localizedDescription)
+            case .failure(let error as NSError):
+                print("receiveMessage: failure [\(error.domain):\(error.code)]")
+                
             case .success(let message):
                 switch message {
                 case .string(let messageString):
@@ -51,8 +55,10 @@ class LocationTrackingService: NSObject, ObservableObject, URLSessionDelegate {
                 default:
                     print("Unknown type received from WebSocket")
                 }
+                // Apple has this weird quirk where this completionHandler
+                // is unregistered every time it fires so we have to re-register
+                self.registerMessageReceiver()
             }
-            self.receiveMessage()
         })
     }
     
@@ -66,21 +72,19 @@ class LocationTrackingService: NSObject, ObservableObject, URLSessionDelegate {
             return
         }
         
-        webSocketTask?.send(URLSessionWebSocketTask.Message.data(jsonData)) { error in
-            if let error = error {
-                print("Failed with Error \(error.localizedDescription)")
-            }
-        }
+        webSocketTask?.send(.data(jsonData), completionHandler: handleError)
     }
     
-    private func closeSocket() {
+    private func closeConnection() {
+        print("closeConnection")
         webSocketTask?.cancel(with: .goingAway, reason: nil)
-        webSocketTask = nil
-        isOpened = false
     }
     
     func beginSendingNearestUpdates() {
         LocationTracker.shared.$beacons.sink() { [self] beacons in
+            if (!isConnected) {
+                return
+            }
             let beacon = beacons
                 .filter { $0.accuracy > 0 } // .accuracy is -1 when undetermined which does not mean near
                 .sorted { $0.accuracy < $1.accuracy } // sort by nearest
@@ -111,21 +115,96 @@ class LocationTrackingService: NSObject, ObservableObject, URLSessionDelegate {
             return
         }
         
-        webSocketTask?.send(.data(jsonData)) { error in
-            if let error = error {
-                print("sendPrecise failed with error: \(error.localizedDescription)")
-            }
-        }
+        webSocketTask?.send(.data(jsonData), completionHandler: handleError)
     }
     
     func beginSendingPreciseLocationUpdates() {
         LocationTracker.shared.$beacons.sink() { [self] beacons in
+            if (!isConnected) {
+                return
+            }
             let beacons = beacons
                 .filter { $0.accuracy > 0 }
                 .map { (Int(truncating: $0.minor), $0.accuracy) }
             
             self.sendPrecise(beacons)
         }.store(in: &subscriptions)
+    }
+    
+    private func handleError(_ error: Error?) {
+        guard let error = error as NSError? else {
+            return
+        }
+        
+        func printHandledError(_ error: NSError) {
+            print("Handling error [\(error.domain):\(error.code)]")
+        }
+        
+        func printUnhandledError(_ error: NSError) {
+            print("Unhandled error [\(error.domain):\(error.code)] \(error.localizedDescription)")
+        }
+        
+        switch error.domain {
+        case NSPOSIXErrorDomain:
+            switch Int32(error.code) {
+            case ENOTCONN, ECONNRESET, ETIMEDOUT: // These are defined in errno.h
+                printHandledError(error)
+            default:
+                printUnhandledError(error)
+            }
+        case NSURLErrorDomain: // These are defined in NSURLError.h
+            switch error.code {
+            case NSURLErrorCannotConnectToHost:
+                printHandledError(error)
+            default:
+                printUnhandledError(error)
+            }
+        default:
+            printHandledError(error)
+        }
+        
+        // We were always going to the same place.
+        // I thought the above might be useful in a more robust app
+        // which handles different errors more intelligently.0
+        isConnected = false
+        closeConnection()
+        
+        // Here we'll try and reconnect with a pseudo-exponential-backoff
+        DispatchQueue.main.async {
+            self.startAttemptingToReconnect()
+        }
+        
+    }
+    
+    private func startAttemptingToReconnect() {
+        print("attempting to reconnect...")
+        isAttemptingReconnect = true
+        
+        
+        // Avoid creating another timer if there's already one running
+        if self.reconnectTimer?.isValid ?? false {
+            return
+        }
+        
+        self.reconnectTimer = Timer.scheduledTimer(withTimeInterval: untilNextAttempt,
+                                              repeats: false) { timer in
+            // If connected we can stop attempting to reconnect
+            if self.isConnected {
+                return
+            }
+            
+            // psuedo-exponential-backoff
+            self.untilNextAttempt *= 2
+            if self.untilNextAttempt > self.maxTimeToWaitBeforeNextAttempt {
+                self.untilNextAttempt = 1
+            }
+            
+            self.connect()
+            
+            print("waiting \(self.untilNextAttempt)s until next attempt")
+            self.startAttemptingToReconnect()
+        }
+    
     }
 }
 
@@ -134,7 +213,9 @@ extension LocationTrackingService: URLSessionWebSocketDelegate {
                     webSocketTask: URLSessionWebSocketTask,
                     didOpenWithProtocol protocol: String?) {
         print("Web socket opened")
-        isOpened = true
+        isConnected = true
+        isAttemptingReconnect = false
+        reconnectTimer?.invalidate()
     }
     
     func urlSession(_ session: URLSession,
@@ -142,6 +223,10 @@ extension LocationTrackingService: URLSessionWebSocketDelegate {
                     didCloseWith closeCode: URLSessionWebSocketTask.CloseCode,
                     reason: Data?) {
         print("Web socket closed")
-        isOpened = false
+        isConnected = false
+    }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        handleError(error)
     }
 }
