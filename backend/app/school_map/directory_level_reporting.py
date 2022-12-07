da233ceff981dc8a89e0c11953423eaa15437a39
17rma <74030061+17rma@users.noreply.github.com>
2022-12-06 22:33:50 -0500

    Updating Status of Students

--- backend/app/school_map/directory_level_reporting.py
+++ backend/app/school_map/directory_level_reporting.py
@@ -18,7 +18,49 @@ def compute_students_near_rooms():
     students = Student.objects.all()
     for s in students:
         if s.beacon_minor_closest_to:
-            if s.beacon_minor_closest_to in to_room:
-                room_counts[to_room[s.beacon_minor_closest_to]] += 1
+            room_counts[to_room[s.beacon_minor_closest_to]] += 1
 
     return room_counts
+
+
+def compute_total_students():
+
+    num_Of_Students = 0
+    students = Student.objects.all()
+    for s in students:
+        num_Of_Students += 1
+
+    return num_Of_Students
+
+
+def compute_medical_students():
+
+    num_med = 0
+    students = Student.objects.all()
+    for s in students:
+        if s.user_reported_status == "requesting-medical":
+            num_med += 1
+
+    return num_med
+
+
+def compute_safe_students():
+
+    num_safe = 0
+    students = Student.objects.all()
+    for s in students:
+        if s.user_reported_status == "is-safe":
+            num_safe += 1
+
+    return num_safe
+
+
+def compute_danger_students():
+
+    num_danger = 0
+    students = Student.objects.all()
+    for s in students:
+        if s.user_reported_status == "sees-shooter":
+            num_danger += 1
+
+    return num_danger
